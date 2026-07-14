41831925eec602045b6495703fa0bb21562e373e
Mohammed Alama <mohammedalsayed9688@gmail.com>
2026-07-14 10:25:57 +0300

    feat: build SymbolTable from ArazzoDocument

---
+++ src/Expression/WorkflowSymbols.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+namespace Alama\LaravelArazzo\Expression;
+
+final readonly class WorkflowSymbols
+{
+    /**
+     * @param array<string,true>              $inputs
+     * @param array<string,true>              $parameters
+     * @param array<string,StepSymbols>       $stepsById
+     * @param array<string,true>              $outputs
+     * @param array<string,true>              $dependsOn
+     */
+    public function __construct(
+        public array $inputs,
+        public array $parameters,
+        public array $stepsById,
+        public array $outputs,
+        public array $dependsOn,
+    ) {}
+}
