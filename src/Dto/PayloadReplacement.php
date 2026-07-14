f1db3305b97ddf3b28b916e2b0fb77662ea8fab1
Mohammed Alama <mohammedalsayed9688@gmail.com>
2026-07-14 09:12:30 +0300

    feat: add leaf DTOs and enums

---
+++ src/Dto/PayloadReplacement.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+namespace Alama\LaravelArazzo\Dto;
+
+final readonly class PayloadReplacement
+{
+    public function __construct(
+        public string $target,
+        public mixed $value,
+    ) {}
+}
