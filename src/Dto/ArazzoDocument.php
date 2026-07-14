ab40ad8e582205a24c816c81c3913dbe5764f768
Mohammed Alama <mohammedalsayed9688@gmail.com>
2026-07-14 09:25:19 +0300

    feat: add container DTOs (Step, Workflow, Components, ArazzoDocument)

---
+++ src/Dto/ArazzoDocument.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+namespace Alama\LaravelArazzo\Dto;
+
+final readonly class ArazzoDocument
+{
+    /**
+     * @param list<SourceDescription> $sourceDescriptions
+     * @param list<Workflow>          $workflows
+     * @param array<string,mixed>     $specificationExtensions
+     */
+    public function __construct(
+        public string $arazzo,
+        public Info $info,
+        public array $sourceDescriptions,
+        public array $workflows,
+        public Components $components,
+        public array $specificationExtensions,
+    ) {}
+}
---
+++ src/Dto/Step.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+namespace Alama\LaravelArazzo\Dto;
+
+use Alama\LaravelArazzo\Dto\Action\FailureAction;
+use Alama\LaravelArazzo\Dto\Action\SuccessAction;
+
+final readonly class Step
+{
+    /**
+     * @param list<Parameter>                        $parameters
+     * @param list<SuccessCriterion>                 $successCriteria
+     * @param list<SuccessAction|Reusable>           $onSuccess
+     * @param list<FailureAction|Reusable>           $onFailure
+     * @param array<string,Expression>               $outputs
+     */
+    public function __construct(
+        public string $stepId,
+        public ?string $description,
+        public ?string $operationId,
+        public ?string $operationPath,
+        public ?string $workflowId,
+        public array $parameters,
+        public ?RequestBody $requestBody,
+        public array $successCriteria,
+        public array $onSuccess,
+        public array $onFailure,
+        public array $outputs,
+    ) {}
+}
