d50e6198bcf7ab5a57ab8c5aebce6b977a1060e0
Mohammed Alama <mohammedalsayed9688@gmail.com>
2026-07-14 10:25:57 +0300

    feat: build SymbolTable from ArazzoDocument

---
+++ src/Expression/StepSymbols.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+namespace Alama\LaravelArazzo\Expression;
+
+final readonly class StepSymbols
+{
+    /** @param array<string,true> $outputs */
+    public function __construct(public array $outputs, public int $index) {}
+}
