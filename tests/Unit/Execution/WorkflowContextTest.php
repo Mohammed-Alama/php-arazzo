6189d7a87aa0e5e10149ed727a78301be41fa513
Mohammed Alama <mohammedalsayed9688@gmail.com>
2026-07-20 06:07:49 +0300

    feat: add immutable workflow context

---
+++ tests/Unit/Execution/WorkflowContextTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Tests\Unit\Execution;
+
+use Alama\LaravelArazzo\Execution\WorkflowContext;
+use PHPUnit\Framework\TestCase;
+
+class WorkflowContextTest extends TestCase
+{
+    public function test_immutability(): void
+    {
+        $context = new WorkflowContext('def_1', ['id' => 1]);
+        $newContext = $context->withStepResult('step_1', ['success' => true]);
+        
+        $this->assertNotSame($context, $newContext);
+        $this->assertEmpty($context->getSteps());
+        $this->assertEquals(['success' => true], $newContext->getSteps()['step_1']);
+        $this->assertEquals('def_1', $newContext->getDefinitionId());
+    }
+}
