2de05e6975d337e20f7f5b973c6136ba997fb7c3
Mohammed Alama <mohammedalsayed9688@gmail.com>
2026-07-20 21:21:17 +0300

    feat: implement Psr18HttpClient adapter and complete service provider wiring

---
+++ src/Laravel/Psr18HttpClient.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alama\LaravelArazzo\Laravel;
+
+use Alama\LaravelArazzo\Execution\Contracts\HttpClientInterface;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+final class Psr18HttpClient implements HttpClientInterface
+{
+    public function __construct(private ClientInterface $client)
+    {
+    }
+
+    public function sendRequest(RequestInterface $request): ResponseInterface
+    {
+        return $this->client->sendRequest($request);
+    }
+}
