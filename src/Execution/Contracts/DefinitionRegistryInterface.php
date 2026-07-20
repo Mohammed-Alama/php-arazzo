1614baf4f7fe3c39d7de71f7a448ac1ed555af6b
Mohammed Alama <mohammedalsayed9688@gmail.com>
2026-07-20 06:34:14 +0300

    feat: implement DefinitionRegistry for versioning

---
+++ src/Execution/Contracts/DefinitionRegistryInterface.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+namespace Alama\LaravelArazzo\Execution\Contracts;
+
+use Alama\LaravelArazzo\Dto\Workflow;
+
+interface DefinitionRegistryInterface
+{
+    public function register(Workflow $workflow): string;
+    public function get(string $definitionId): ?Workflow;
+}
