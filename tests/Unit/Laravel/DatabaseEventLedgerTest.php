3e312f452c101571e5d66c41879c75b6a1860d07
Mohammed Alama <mohammedalsayed9688@gmail.com>
2026-07-20 06:33:47 +0300

    feat: implement DatabaseEventLedger

---
+++ tests/Unit/Laravel/DatabaseEventLedgerTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Tests\Unit\Laravel;
+
+use Alama\LaravelArazzo\Laravel\DatabaseEventLedger;
+use Illuminate\Database\ConnectionInterface;
+use Illuminate\Database\Query\Builder;
+use PHPUnit\Framework\TestCase;
+
+class DatabaseEventLedgerTest extends TestCase
+{
+    public function test_appends_event_to_database(): void
+    {
+        $builder = $this->createMock(Builder::class);
+        $builder->expects($this->once())->method('insert')->willReturn(true);
+        
+        $db = $this->createMock(ConnectionInterface::class);
+        $db->method('table')->with('arazzo_events')->willReturn($builder);
+        
+        $ledger = new DatabaseEventLedger($db, 'arazzo_events');
+        $ledger->append('wf_1', 'StepExecuted', ['stepId' => 'A']);
+    }
+}
